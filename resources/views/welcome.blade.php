0b7bcd2c168d8f47ed434fb8638852caf04023a3
Gracia Nathania <gracianathania15@gmail.com>
2026-09-09 16:08:41 +0700

    fix: hardcode HTML title to CGIS Pekanbaru

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>{{ config('app.name', 'CGIS Pekanbaru') }}</title>
+    <title>CGIS Pekanbaru</title>
     <link rel="icon" type="image/png" href="{{ asset('images/logo_C-GIS.png') }}">
 
     <!-- Fonts -->
