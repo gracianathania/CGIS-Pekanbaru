8a2444b2d8222f200aabc294aaab25fa64028e35
Gracia Nathania <gracianathania15@gmail.com>
2026-09-09 16:05:09 +0700

    feat: add C-GIS favicon logo to all page tabs

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <title>{{ config('app.name', 'CGIS Pekanbaru') }}</title>
+    <link rel="icon" type="image/png" href="{{ asset('images/logo_C-GIS.png') }}">
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
